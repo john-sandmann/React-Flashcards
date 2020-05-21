b0750723915c941ed0579a55e7b5d15a06237f23
Joao Areno <joao.areno2006@gmail.com>
2020-05-21 14:49:31 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import DisplayCards from './Pages/DisplayCards.js';
+import ManageCards from './Pages/ManageCards.js';
+
+import './Sass/index.scss';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={DisplayCards}/>
+      <Route exact path="/ManageCards" component={ManageCards}/>
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
